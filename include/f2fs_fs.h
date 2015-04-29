cefbc4f8cf92cf560199ff3e0c1dcd7cd7bf0533
Andrew Aldridge <i80and@foxquill.com>
2015-04-28 21:02:07 -0400

    Remove linux dependency

--- include/f2fs_fs.h
+++ include/f2fs_fs.h
@@ -13,8 +13,18 @@
 #define __F2FS_FS_H__
 
 #include <inttypes.h>
-#include <linux/types.h>
 #include <sys/types.h>
+
+typedef uint16_t __u16;
+typedef uint8_t __u8;
+typedef uint32_t __u32;
+typedef uint64_t __u64;
+typedef __u16 __le16;
+typedef __u16 __be16;
+typedef __u32 __le32;
+typedef __u32 __be32;
+typedef __u64 __le64;
+typedef __u64 __be64;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
